938ae81d506f332579916d438585dc79f73bb4f7
Bruhdothis <bruhtavares1982@gmail.com>
2025-05-21 18:39:24 -0300

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,6 +6,63 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
+    #include <stdio.h>
+
+int main() {
+    char nomeA[50], nomeB[50];
+    int populacaoA, populacaoB, escolha1, escolha2;
+    float areaA, areaB, pibA, pibB;
+
+    printf("Digite o nome da Cidade 1: ");
+    scanf("%s", nomeA);
+    printf("Digite a população da Cidade 1: ");
+    scanf("%d", &populacaoA);
+    printf("Digite a área da Cidade 1: ");
+    scanf("%f", &areaA);
+    printf("Digite o PIB da Cidade 1: ");
+    scanf("%f", &pibA);
+
+    printf("Digite o nome da Cidade 2: ");
+    scanf("%s", nomeB);
+    printf("Digite a população da Cidade 2: ");
+    scanf("%d", &populacaoB);
+    printf("Digite a área da Cidade 2: ");
+    scanf("%f", &areaB);
+    printf("Digite o PIB da Cidade 2: ");
+    scanf("%f", &pibB);
+
+    printf("\nEscolha dois atributos para comparar:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("Digite sua primeira escolha: ");
+    scanf("%d", &escolha1);
+    printf("Digite sua segunda escolha: ");
+    scanf("%d", &escolha2);
+
+    printf("\nComparando %s e %s:\n", nomeA, nomeB);
+
+    printf("No atributo %d, ", escolha1);
+    if ((escolha1 == 1 && populacaoA > populacaoB) || (escolha1 == 2 && areaA > areaB) || (escolha1 == 3 && pibA > pibB)) {
+        printf("%s é melhor.\n", nomeA);
+    } else if ((escolha1 == 1 && populacaoA < populacaoB) || (escolha1 == 2 && areaA < areaB) || (escolha1 == 3 && pibA < pibB)) {
+        printf("%s é melhor.\n", nomeB);
+    } else {
+        printf("As cidades são iguais nesse atributo.\n");
+    }
+
+    printf("No atributo %d, ", escolha2);
+    if ((escolha2 == 1 && populacaoA > populacaoB) || (escolha2 == 2 && areaA > areaB) || (escolha2 == 3 && pibA > pibB)) {
+        printf("%s é melhor.\n", nomeA);
+    } else if ((escolha2 == 1 && populacaoA < populacaoB) || (escolha2 == 2 && areaA < areaB) || (escolha2 == 3 && pibA < pibB)) {
+        printf("%s é melhor.\n", nomeB);
+    } else {
+        printf("As cidades são iguais nesse atributo.\n");
+    }
+
+    return 0;
+}
+
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
 
